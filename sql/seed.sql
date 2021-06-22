41913e50cf9d33f0feaee89fdc74c7d217c1c1e6
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-06-22 18:07:49 -0400

    initial commit for class survey sql exercise

---
+++ sql/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO rankings
+    (ranking_title)
+VALUES
+    ('Awesome'),
+    ('Great'),
+    ('Good'),
+    ('Okay'),
+    ('Poor'),
+    ('Unranked');
+
+INSERT INTO topics
+    (topic_name, topic_score)
+VALUES
+    ('HTML', '2'),
+    ('CSS', '2'),
+    ('Javascript', '4'),
+    ('PostgreSQL', '1'),
+    ('Node', '3'),
+    ('Express', '4');
