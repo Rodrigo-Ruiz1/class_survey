296f1d75cfaf0feaae2cc320c81e81ea8841a26b
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-06-25 12:42:17 -0400

    made changes to allow for updating rankings, created error partial

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,58 @@
 const express = require('express');
+const { errors } = require('pg-promise');
 const router = express.Router();
 const ClassSurveyModel = require('../models/ClassSurveyModel');
 
 router.get('/', async (req, res) => {
     const topicData = await ClassSurveyModel.getAllTopicData();
+    const rankings = await ClassSurveyModel.getAllRankings();
+    // console.log("RANKINGS ARE: ", rankings)
     res.render('template', {
         locals: {
             title: 'Class Survey',
-            data: topicData
+            data: topicData,
+            rankings: rankings
         },
         partials: {
             body: 'partials/home'
         }
     })
 });
+
+router.get('/error', async (req, res) => {
+    const { error } = req.query;
+
+    let errorMessage = error;
+
+    if(!error) {
+        errorMessage = "No Error Message Defined";
+    }
+
+    res.render('template', {
+        locals: {
+            title: "ERROR PAGE",
+            errorMessage: errorMessage,
+        },
+        partials: {
+            body: 'partials/error'
+        }
+    })
+})
+
+router.post('/update', async (req, res) => {
+    let response;
+
+    for (let key in req.body) {
+        console.log("KEY AND VALUE:", key, req.body[key]);
+        const response = await ClassSurveyModel.updateRanking(key, req.body[key]);
+    }
+    if (response.rowCount !== 1) {
+        res.redirect(`/error?error=${response}`);
+    } else {
+        res.redirect('/');
+    }
+    // console.log("RESPONSE IN POST: ", response);
+    res.redirect('/');
+})
 
 module.exports = router;
